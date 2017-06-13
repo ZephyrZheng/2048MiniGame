b62f0eafa3161cf41c8fc6867b2d625a57fea12a
ZephyrZheng <zhengzhifeng77@gmail.com>
2017-06-13 09:28:15 +0800

    2048小游戏版本1.0

---
+++ 2048_mini-master/README.md
@@ -0,0 +1,19 @@
+2048 mini
+====
+
+An open source version of 2048 that runs on Android devices. This project is only for **studying**, do NOT use it for commercial purpose.
+##### Why it calls 2048 mini
+* It copies the idea and only the idea of 2048.
+* The size of the final APK file is small. (32KB for now)
+
+
+##### What it looks like now
+![](https://github.com/DailyEfforts/repo/blob/master/2048_mini.gif)
+
+##### Where to download the APK file
+Please [Click Me](https://github.com/DailyEfforts/repo/blob/master/2048_mini.apk?raw=true) to download the APK file.
+
+##### About the source code
+* It is original.
+* Feel free to fork or download.
+* Feel free to submit patches.
---
+++ 2048_mini-master/app/src/com/gmail/dailyefforts/merge/NumbersGridView.java
@@ -0,0 +1,61 @@
+
+package com.gmail.dailyefforts.merge;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.GridView;
+
+public class NumbersGridView extends GridView {
+
+    private GestureDetector mDetecter;
+    private OnSwipeListener mOnSwipeListener;
+
+    public interface OnSwipeListener {
+        void onSwipe(final int direction);
+    }
+
+    public void setOnChangedListener(OnSwipeListener listener) {
+        mOnSwipeListener = listener;
+    }
+
+    public NumbersGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mDetecter = new GestureDetector(context,
+                new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onFling(MotionEvent e1, MotionEvent e2,
+                            float velocityX, float velocityY) {
+                        final float absX = Math.abs(velocityX);
+                        final float absY = Math.abs(velocityY);
+                        final float deltaX = e2.getX() - e1.getX();
+                        final float deltaY = e2.getY() - e1.getY();
+                        final int travelX = getWidth() / 20;
+                        final int travelY = travelX;
+                        if (absY < absX && deltaX < -travelX) {
+                            mOnSwipeListener.onSwipe(Direction.LEFT);
+                            return true;
+                        } else if (absY < absX && deltaX > travelX) {
+                            mOnSwipeListener.onSwipe(Direction.RIGHT);
+                            return true;
+                        } else if (absX < absY && deltaY > travelY) {
+                            mOnSwipeListener.onSwipe(Direction.DOWN);
+                            return true;
+                        } else if (absX < absY && deltaY < -travelY) {
+                            mOnSwipeListener.onSwipe(Direction.UP);
+                            return true;
+                        }
+                        return false;
+                    }
+                });
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mDetecter.onTouchEvent(event);
+        return true;
+    }
+
+}
